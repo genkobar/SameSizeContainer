b25db608c2f612f380961bdca1861f341175f65d
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2020-11-24 09:46:36 +0000

    Bump minimum iOS version to 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SameSizeContainer",
-    platforms: [.macOS(.v10_15), .iOS(.v12)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         .library(
             name: "SameSizeContainer",
